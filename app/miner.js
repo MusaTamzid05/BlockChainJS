74c629005f117cf523ceaba57eec0cd476b45d62
Musa <tamzid101@gmail.com>
2019-03-26 15:27:05 +0600

    user can mine now and it has a endpoint.

--- app/miner.js
+++ app/miner.js
@@ -11,12 +11,13 @@ class Miner {
     }
 
     mine() {
-
         const validTransactions = this.transactionPool.validTransactions();
         validTransactions.push(Transaction.rewardTransaction(this.wallet , Wallet.blockchainWallet()));
         const block = this.blockchain.addBlock(validTransactions);
         this.p2pServer.syncChains();
         this.transactionPool.clear();
+        this.p2pServer.broadcastClearTransactions();
+        return block;
     }
 }
 
